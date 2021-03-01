08a5d08198def013a22b2f296632d5fc63d77d4d
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2021-03-01 11:06:00 +0700

    Time: 4 ms (23.39%), Space: 5.7 MB (96.28%) - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int res = 0;
+        for (long long i=5; n/i > 0; i*=5){
+            res += (n/i);
+        }
+        return res;
+    }
+};
