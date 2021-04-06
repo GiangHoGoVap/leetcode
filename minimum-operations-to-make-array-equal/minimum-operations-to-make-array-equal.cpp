87030e451979a77edd8b918dea1a55615e2d4380
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2021-04-06 16:28:18 +0700

    Time: 0 ms (100.00%), Space: 5.9 MB (24.42%) - LeetHub

---
+++ minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int helper(int start, int diff,int elements){
+        int ans = 0;
+        for(int i = 0 ; i<elements ; i++){
+            ans += start;
+            start += diff;
+        }
+        return ans;
+    }
+    int minOperations(int n) {
+        if (n % 2 != 0) return helper(2, 2, n/2);
+        return helper(1, 2, n/2);
+    }
+};
