ce2b8173cc74e8d2cbcfb08b62c5d4946d71e6af
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2022-03-27 15:40:10 +0700

    Time: 0 ms (100.00%), Space: 6.2 MB (10.02%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int countSegments(string s) {
         int ans = 0;
         for (int i=0; i<s.length(); i++){
-            if ((s[i] != ' ' && s[i+1] == ' ') ||  (s[i] != ' ' && s[i+1] == NULL)){
+            if ((s[i] != ' ' && s[i+1] == ' ') || (s[i] != ' ' && s[i+1] == NULL)){
                 ans++;
             }
         }
