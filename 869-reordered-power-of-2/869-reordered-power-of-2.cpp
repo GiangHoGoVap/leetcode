1111ce061746055a010debd81df2f8600161bfc5
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2022-08-26 07:43:34 +0700

    Time: 4 ms (61.58%), Space: 6 MB (83.90%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string sorted_num(int n){
+        string res = to_string(n);
+        sort(res.begin(), res.end());
+        return res;
+    }
+    bool reorderedPowerOf2(int N) {
+        string str = sorted_num(N);
+        for (int i = 0; i < 32; i++){
+            if (str == sorted_num(1 << i)) return true;
+        }
+        return false;
+    }
+};
