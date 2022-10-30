ea6369bf19a9df4925ec9a1408e28f444d7d39ed
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2022-10-30 09:37:40 +0700

    Time: 623 ms (19.12%), Space: 73.7 MB (11.83%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int shortestPath(vector<vector<int>> &grid, int k) {
+        int M = grid.size(), N = grid[0].size(), steps = 0;
+        const vector<int> dir{-1, 0, 1, 0, -1};
+        vector<vector<vector<int>>> seen(M, vector<vector<int>>(N, vector<int>(k + 1)));
+        queue<vector<int>> q{{{0, 0, 0}}};
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz-- > 0) {
+                auto p = q.front();
+                q.pop();
+                if (p[0] == M - 1 && p[1] == N - 1) return steps;
+                for (int i = 0; i < 4; i++) {
+                    int x = p[0] + dir[i], y = p[1] + dir[i + 1];
+                    if (x < 0 || x >= M || y < 0 || y >= N) continue;
+                    int w = grid[x][y] + p[2];
+                    if (w > k || seen[x][y][w]) continue;
+                    seen[x][y][w] = 1;
+                    q.push({x, y, w});
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
